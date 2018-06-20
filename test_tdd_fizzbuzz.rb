51c053e1bfccd4b1b3e841b12c21fc95ac68e5bb
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 14:08:07 -0400

    Third position is mined failure test

--- test_tdd_fizzbuzz.rb
+++ test_tdd_fizzbuzz.rb
@@ -21,6 +21,6 @@ class TestTddFizzBuzz < Minitest::Test
         assert_equal(1, fizzbuzz().first)
     end
     def test_assert_third_position_is_mined
-        assert_equal(mined, fizzbuzz()[3])
+        assert_equal("mined", fizzbuzz()[2])
     end
  end
