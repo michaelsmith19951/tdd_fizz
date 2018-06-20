8dc72b5efcfa08cecf222b45195c3b628a3f3f02
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 13:49:35 -0400

    Added array and class

--- test_tdd_fizzbuzz.rb
+++ test_tdd_fizzbuzz.rb
@@ -9,4 +9,8 @@ class TestTddFizzBuzz < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_assert_that_end_result_is_array
+        assert_equal(Array, fizzbuzz().class)
+    end
  end
