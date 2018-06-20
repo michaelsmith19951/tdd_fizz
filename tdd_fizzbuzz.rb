9fa981a9cde2e11cc82522e13df755607656f99a
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 15:07:22 -0400

    Added if multiples of 3, return mined

--- tdd_fizzbuzz.rb
+++ tdd_fizzbuzz.rb
@@ -1,8 +1,15 @@
 
-def fizzbuzz
+def fizzbuzz()
 	fizzarr = [*1..100]
 	fizzarr[2] = "mined"
 	fizzarr[4] = "minds"
 	fizzarr[14] = "mined minds"
-	fizzarr
+	new_arr = []
+	fizzarr.each do |item|
+	if item % 3 == 0
+		item = "mined"
+		end
+		new_arr << item
+	end
+	new_arr
 end
