9aa157493953f15304c1988c9b8f8e6364d588b3
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 14:20:04 -0400

    'test_assert_fifth_position_is_mines' failure

--- test_tdd_fizzbuzz.rb
+++ test_tdd_fizzbuzz.rb
@@ -23,4 +23,7 @@ class TestTddFizzBuzz < Minitest::Test
     def test_assert_third_position_is_mined
         assert_equal("mined", fizzbuzz()[2])
     end
+    def test_assert_fifth_position_is_mines
+        assert_equal("mines", fizzbuzz()[4])
+    end
  end
