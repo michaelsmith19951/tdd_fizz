7603a31eed3e6304b4945ce00a3edf830f14ee17
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 11:52:09 -0400

    Added Testtdd_fizz, third commit

--- test_tdd_fizz.rb
+++ test_tdd_fizz.rb
@@ -3,11 +3,13 @@ require "minitest/autorun"
 # Use the file that will be tested
 require_relative "tdd_fizz.rb"
 
-# Test class
+# Test the Testtdd_fizz
 class TestTdd_fizz < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-
+    def test_result_is_array
+    	assert_equal(Array, fizzbuzz().class)
+    end
 end
